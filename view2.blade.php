12455256d94cfb86ebbf9809ca6b37448bb4f21d
nadiapramesti <53283695+nadiapramesti@users.noreply.github.com>
2020-09-07 15:47:23 +0700

    Create view2.blade.php

---
+++ view2.blade.php
@@ -0,0 +1,21 @@
+<title>Percobaan view 2</title>
+<div class="container">
+<h1>DAFTAR SISWA SMKN 1 SUBANG</h1>
+<table id="tabel_siswa" class="table table-bordered table-striped table-hover">
+<table border="1" width="100%">
+	<thead>
+		<tr>
+			<th>No</th>
+			<th>Nis</th>
+			<th>Nama</th>
+			<th>Jk</th>
+			<th>Tempat lahir</th>
+			<th>Tanggal Lahir</th>
+			<th>Kelas</th>
+			<th>No hp</th>
+			<th>Tanggal Entri</th>
+			<th width="150px">Aksi</th>
+		</tr>
+	</thead>
+</table>
+</div>
