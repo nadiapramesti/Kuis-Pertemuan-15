cb9a0e82ee46acc782c7052fe7d786ee6896b136
nadiapramesti <53283695+nadiapramesti@users.noreply.github.com>
2020-09-07 16:15:55 +0700

    Create view3.blade.php

---
+++ view3.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link href="https://fonts.googleapis.com/css?family=Lato:300&display=swap" rel="stylesheet"> 
+</head>
+<body>
+        	<div class="row">
+        		<h2>We're happy to hear from you</h2>
+        	</div>
+
+        	<div class="row">
+        		<form method="post" action="#" class="contac-form">
+        			<div class="row">
+        				<div class="col span-1-of-3">
+        					<label for="name">Name</label>
+        				</div>
+
+        				<div class="col span-2-of-3">
+        					<input type="text" name="name" placeholder="your name" required>
+        				</div>
+        			</div>
+
+        			<div class="row">
+        				<div class="col span-1-of-3">
+        					<label for="email">Email</label>
+        				</div>
+
+        				<div class="col span-2-of-3">
+        					<input type="email" name="email" placeholder="your email" required>
+        				</div>
+        			</div>
+
+        			<div class="row">
+        				<div class="col span-1-of-3">
+        					<label for="find-us">How did you find us?</label>
+        				</div>
+
+        				<div class="col span-2-of-3">
+        					<select name="find-us" id="find-us">
+        						<option value="friend" selected>Friends</option>
+        						<option value="search" selected>Search Engine</option>
+        						<option value="ad" selected>Advertisement</option>
+        						<option value="other" selected>Other</option>
+        					</select>
+        				</div>
+        			</div>
+
+        			<div class="row">
+        				<div class="col span-1-of-3">
+        					<label for="news">Newsletter</label>
+        				</div>
+
+        				<div class="col span-2-of-3">
+        					<input type="checkbox" name="news" id="news" checked=""> Yes,please
+        				</div>
+        			</div>
+
+        			<div class="row">
+        				<div class="col span-1-of-3">
+        					<label for="message">Drop us a line</label>
+        				</div>
+
+        				<div class="col span-2-of-3">
+        					<textarea name="message" id="message" placeholder="your message"></textarea>
+        				</div>
+        			</div>
+
+        			<div class="row">
+        				<div class="col span-1-of-3">
+        					<div></div>
+        				</div>
+
+        				<div class="col span-2-of-3">
+        					<input type="submit" name="" value="Send it!">
+        				</div>
+        			</div>
+        		</form>
+        	</div>
+</body>
+</html>
