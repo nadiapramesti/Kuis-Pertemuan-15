ee5daca649849855f7af6fadc8982755fac593a0
nadiapramesti <53283695+nadiapramesti@users.noreply.github.com>
2020-09-07 15:32:27 +0700

    Create view1.blade.php

---
+++ view1.blade.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+  <title>Percobaan view 1</title>
+</head>
+<body>
+  <h1>Data yang di perlukan:</h1>
+  <ol>
+    <li>Foto</li>
+    <li>NIS</li>
+    <li>Nama</li>
+    <li>Jenis Kelamin</li>
+    <li>NO.Telepon</li>
+    <li>Alamat</li>
+  </ol>
+</body>
+</html>
